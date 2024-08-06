c2c1c2db1040fe13edb0db298eeffcc87c81fcff
SARAN S M <saran007mgp@gmail.com>
2024-08-06 17:52:20 +0530

    updated url for cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://invoice-portal-frontend.vercel.app'],
     methods: ['GET', 'POST', 'PUT'],
     credentials: false,
 }));
